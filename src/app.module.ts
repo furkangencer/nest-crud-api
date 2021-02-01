cc0e25e84c8f3dd09b6d03164d4d1fb5a22b984d
Furkan Gençer <furkan.gencer@getir.com>
2021-02-02 02:32:39 +0300

    Add config module to load .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot(process.env.MONGODB_URI)],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
